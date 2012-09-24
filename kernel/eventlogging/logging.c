807a5df0356416acf341bc5b7ac382cfa761c56e
David R. Bild <drbild@umich.edu>
2012-09-24 07:34:44 -0400

    eventlogging: change from double buffers to buffer queues.

--- kernel/eventlogging/logging.c
+++ kernel/eventlogging/logging.c
@@ -1,29 +1,85 @@
 #include <linux/cpumask.h>
 #include <linux/percpu.h>
-#include <linux/gfp.h>
+#include <linux/slab.h>
 
+#include "logging.h"
 #include "buffer.h"
 #include "proc_fs.h"
 
-#define BUFFER_ORDER 12 // 2^12 = 16 MB with 4096 page size
+// 2^12 = 16 MB with 4096 page size
+#define BUFFER_ORDER 2
 
-static DEFINE_PER_CPU(struct dbuffer, buffers);
+static DEFINE_PER_CPU(struct sbuffer*, sbuffers);
+static DEFINE_PER_CPU(unsigned int, missed_events);
+
+static struct sbuffer* __get_cpu_buffer(void) {
+  struct sbuffer* buf = __get_cpu_var(sbuffers);
+  if (NULL == buf) {
+    buf = take_empty_try();
+    __get_cpu_var(sbuffers) = buf;
+  }
+  return buf;
+}
+
+static struct sbuffer* __flush_cpu_buffer(void) {
+  put_full(__get_cpu_var(sbuffers));
+  __get_cpu_var(sbuffers) = NULL;
+  return __get_cpu_buffer();
+}
+
+void log_event(void* data, int len) {
+  struct sbuffer* buf;
+  unsigned long avail;
+
+  // Get buffer, if available
+  buf = __get_cpu_buffer();
+  if (NULL == buf) {
+    __get_cpu_var(missed_events)++;
+    return;
+  }
+
+  // Flush and replace buffer, if not enough room for new event
+  avail = sbuffer_avail(buf);
+  if (avail < len) {
+    buf = __flush_cpu_buffer();
+    if (NULL == buf) {
+      __get_cpu_var(missed_events)++;
+      return;
+    }
+  }
+  
+  // Write data
+  sbuffer_write(buf, data, len);
+}
 
 static __init int init_alloc_buffers(void) {
   int ret = 0;
 
   int cpu;
   for_each_cpu(cpu, cpu_possible_mask) {
-    ret = dbuffer_init(&per_cpu(buffers, cpu), BUFFER_ORDER);
-    if (ret)
-      goto err;
+    // Create two buffers, one to use now and one for the empty list
+    int i;
+    struct sbuffer* buf[2];
+    for (i = 0; i < 2; ++i) {
+      buf[i] = (struct sbuffer*) kmalloc(sizeof(struct sbuffer), GFP_ATOMIC);
+      if (0 == buf[i]) {
+	ret = -ENOMEM;
+	goto err;
+      }
+      sbuffer_init(buf[i], BUFFER_ORDER);
+    }
 
-    printk("eventlogging: Allocated buffer for CPU %d\n", cpu);
+    // Attach buffers
+    per_cpu(sbuffers, cpu) = buf[0];
+    put_empty(buf[1]);
+
+    printk("eventlogging: allocated buffers for CPU %d\n", cpu);
   }
+
   return 0;
 
  err:
-  printk("eventlogging: Failed to allocate buffer for CPU %d\n", cpu);
+  printk("eventlogging: failed to allocate buffer for CPU %d\n", cpu);
   return ret;
 }
 
