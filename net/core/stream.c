3e5167eba037a99803bb40fea96711157048fd85
David R. Bild <drbild@umich.edu>
2012-09-24 14:14:47 -0400

    eventlogging: add per-event config options.

--- net/core/stream.c
+++ net/core/stream.c
@@ -19,9 +19,7 @@
 #include <linux/wait.h>
 #include <net/sock.h>
 
-#ifdef CONFIG_EVENT_LOGGING
 #include <eventlogging/events.h>
-#endif
 
 /**
  * sk_stream_write_space - stream socket write_space callback.
@@ -75,16 +73,12 @@ int sk_stream_wait_connect(struct sock *sk, long *timeo_p)
 
 		prepare_to_wait(sk_sleep(sk), &wait, TASK_INTERRUPTIBLE);
 		sk->sk_write_pending++;
-#ifdef CONFIG_EVENT_LOGGING
 		event_log_stream_block();
-#endif
 		done = sk_wait_event(sk, timeo_p,
 				     !sk->sk_err &&
 				     !((1 << sk->sk_state) &
 				       ~(TCPF_ESTABLISHED | TCPF_CLOSE_WAIT)));
-#ifdef CONFIG_EVENT_LOGGING
 		event_log_stream_resume();
-#endif
 		finish_wait(sk_sleep(sk), &wait);
 		sk->sk_write_pending--;
 	} while (!done);
